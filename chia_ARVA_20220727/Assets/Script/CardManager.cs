8eebcdb521c4a56d45c7c77b8c694ed1c623e5df
j15935720033 <99215381@ulive.pccu.edu.tw>
2022-07-27 14:57:48 +0800

    update

--- chia_ARVA_20220727/Assets/Script/CardManager.cs
+++ chia_ARVA_20220727/Assets/Script/CardManager.cs
@@ -1,6 +1,9 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
 namespace chia.AR.Vuforia
 {
 
@@ -11,18 +14,35 @@ namespace chia.AR.Vuforia
     {
         [SerializeField, Header("KID圖片辨識")]
         private DefaultObserverEventHandler observerKID;
+        [SerializeField, Header("小騎士")]
+        private Animator aniKnight;
+        [SerializeField, Header("攻擊按鈕")]
+        private Button btnAttack;
+        [SerializeField, Header("虛擬按鈕跳躍")]
+        private VirtualButtonBehaviour vbbJump;
 
+        private string parVictory = "觸發勝利";
         private void Awake()
         {
-            observerKID.OnTargetFound.AddListener(CardFound);//.AddListener(方法)
-            observerKID.OnTargetLost.AddListener(CardLost);
+            observerKID.OnTargetFound.AddListener(CardFound);//目標找到事件   //.AddListener(方法)
+            observerKID.OnTargetLost.AddListener(CardLost);//目標遺失事件
+            btnAttack.onClick.AddListener(Attack);//按鈕點擊事件
+            vbbJump.RegisterOnButtonPressed(OnJmpPressed);
+
         }
+
+        private void OnJmpPressed(VirtualButtonBehaviour obj)
+        {
+            print("<color=#3366dd>跳躍<color>");
+        }
+
         /// <summary>
         /// 圖片辨識成功
         /// </summary>
         private void CardFound()
         {
             print("<color=yellow>找到卡片</color>");
+            aniKnight.SetTrigger(parVictory);
         }
         /// <summary>
         /// 圖片辨識失敗
@@ -30,6 +50,10 @@ namespace chia.AR.Vuforia
         private void CardLost()
         {
             print("<color=red>卡片辨識失敗</color>");
+        }
+        private void Attack()
+        {
+            print("<color=#9955aa>攻擊!!!</color>");
         }
     }
 }
